61e2b1e5fb2094eab1939540aed64b329446799e
clabra <clabra@gmail.com>
2024-09-20 13:55:23 +0200

    Prepare environment

---
+++ run.sh
@@ -0,0 +1,2 @@
+python3 train_link_classification.py --dataset_name assist17 --model_name DyGKT --num_neighbor 100 --num_runs 5 --gpu 0
+#python3 evaluate_link_classification.py --dataset_name assist17 --model_name DyGKT --num_neighbor 100 --num_runs 5 --gpu 0
